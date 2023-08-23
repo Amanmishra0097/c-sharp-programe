38c16db7aee875dc35820cd345b951e046356819
Amanmishra0097 <sultan0097198@gmail.com>
2023-08-23 23:21:41 +0530

    done with the code

---
+++ Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace manish
+{
+    partial class Program
+    {
+        public void k()
+        {
+            Console.WriteLine("second class manish function");
+        }
+        public void pachauri()
+        {
+            Console.WriteLine("hi pachauri:");
+        }
+
+    }
+}
+
+
+
